369b66b3b5933dfdd14eb8dbd5c2e43af57c494d
Phan Đức Việt <viet@techmaster.vn>
2017-06-02 00:11:45 +0700

    bo sung splash screen

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -11,15 +11,16 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, loadingScreen;
 
 function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 500, height: 540,
         resizable: false,
+        show: false,
         frame: false,
-        icon: path.join(__dirname, '/../public/img/logo.jpg')
+        icon: path.join(__dirname, '/../public/png/logo.png')
     })
 
     // and load the index.html of the app.
@@ -29,6 +30,16 @@ function createWindow() {
             slashes: true
         });
     mainWindow.loadURL(startUrl);
+
+    mainWindow.webContents.on('did-finish-load', () => {
+        mainWindow.show();
+
+        if (loadingScreen) {
+            let loadingScreenBounds = loadingScreen.getBounds();
+            mainWindow.setBounds(loadingScreenBounds);
+            loadingScreen.close();
+        }
+    });
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
@@ -42,10 +53,31 @@ function createWindow() {
     })
 }
 
+function createLoadingScreen() {
+    loadingScreen = new BrowserWindow({
+        width: 500,
+        height: 500,
+        resizable: false,
+        show: false,
+        frame: false,
+        icon: path.join(__dirname, '/../public/png/logo.png'),
+        parent: mainWindow
+    });
+    const url = process.env.ELECTRON_START_URL ? path.join(__dirname, '/../public/loading.html') : path.join(__dirname, '/../build/loading.html')
+    loadingScreen.loadURL('file://' + url);
+    loadingScreen.on('closed', () => loadingScreen = null);
+    loadingScreen.webContents.on('did-finish-load', () => {
+        loadingScreen.show();
+    });
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+    createLoadingScreen();
+    createWindow();
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
